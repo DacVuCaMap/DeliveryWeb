a01451f6d1b2e3a5f3e24e613dd89b01aa32fe81
DacVuCaMap <namvu04112k@gmail.com>
2025-04-02 10:57:22 +0700

    up

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -3,8 +3,8 @@ import SignInForm from "@/components/temp/auth/SignInForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "SignIn Page",
+  description: "This is Next.js ",
 };
 
 export default function SignIn() {
